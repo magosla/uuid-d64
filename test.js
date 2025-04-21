1bf4965c9c44ab87d8737adfca64d82777b189fe
Magnus Lator <magosla2001@gmail.com>
2025-04-21 01:50:31 +0100

    add test for uuid v7

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 const test = require('ava');
 const uuid = require('uuid').v4;
+const uuid7 = require('uuid').v7;
 const { encode, decode } = require('.');
 
 test('decoded data integrity', t => {
   const id = uuid();
+  const encoded = encode(id);
+  const decoded = decode(encoded);
+
+  t.is(id, decoded);
+});
+
+
+test('decoded data integrity for uuid 7', (t) => {
+  const id = uuid7();
   const encoded = encode(id);
   const decoded = decode(encoded);
 
